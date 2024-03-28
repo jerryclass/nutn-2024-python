e4051441eba2fb6f4239e0113a74deb67ff18f62
jerryclass <jerryclass@gmail.com>
2024-03-28 14:49:23 +0800

    no message

---
+++ 利用 Dockerfile 建立 images/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMAGE_NAME="nutn-my-python"
+
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+	echo "Docker image $IMAGE_NAME not found. Building..."
+	docker build -f Docker/Dockerfile -t $IMAGE_NAME .
+fi
+
+docker run -it --rm -v "$(pwd):/app" $IMAGE_NAME python /app/hello.py
